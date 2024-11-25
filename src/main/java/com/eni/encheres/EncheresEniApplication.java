4338da5b145f89d22f9972fbbd430545c0b524a5
AliceDesgranges <alice.desgranges@apside.com>
2024-11-25 10:32:25 +0100

    first commit

---
+++ src/main/java/com/eni/encheres/EncheresEniApplication.java
@@ -0,0 +1,13 @@
+package com.eni.encheres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheresEniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncheresEniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eni/encheres/EncheresEniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eni.encheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheresEniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
