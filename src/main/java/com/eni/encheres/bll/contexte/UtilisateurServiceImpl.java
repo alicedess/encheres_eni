7a71bdbd01dcb649ebcbb553b1af99516097aad5
hmarneth <hmarneth@phenixdigital.fr>
2024-11-28 16:51:12 +0100

    Primary bean kicked out

--- src/main/java/com/eni/encheres/bll/contexte/UtilisateurServiceImpl.java
+++ src/main/java/com/eni/encheres/bll/contexte/UtilisateurServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
 public class UtilisateurServiceImpl implements UtilisateurService {
 
     private UtilisateurDAO utilisateurDAO;
